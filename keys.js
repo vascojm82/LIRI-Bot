73da10bc93bfc0248db6667a8bb9a94a46ad51f8
Jose M. Vasconcellos <miguel_vasconce@hotmail.com>
2018-10-10 16:43:04 -0400

    LIRI BOT APP

---
+++ keys.js
@@ -0,0 +1,11 @@
+// console.log('Keys are loaded');
+
+exports.credentials = {
+  spotify : {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  },
+  imdb : {
+    key: process.env.IMDB_KEY
+  }
+}
